97d92a8923b01bf269fc54fd96a00bf488d8715a
Ovallediaz <88749116+Ovallediaz@users.noreply.github.com>
2021-08-16 18:27:42 -0500

    he creado carpeta con nombre

--- Assets/LUIS OVALLE/Hola.cs
+++ Assets/LUIS OVALLE/Hola.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Borrar : MonoBehaviour
+public class Hola : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
